13736f05703efdf9e55af1a7a7f55bbb4c1b1411
Mohd Anas Kamal <mohdanaskamal810@gmail.com>
2024-05-12 20:29:13 +0530

    add

---
+++ react-state-class_2/src/App.jsx
@@ -0,0 +1,41 @@
+
+// import './App.css'
+// import LudoBoard from './LudoBoard'
+
+// function App() {
+  
+
+//   return (
+//     <>
+//       <LudoBoard/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+// Activity : TodoList
+
+import './App.css'
+import LudoBoard from './LudoBoard'
+import TodoList from './TodoList'
+
+function App() {
+  
+
+  return (
+    <>
+      <TodoList/>
+    </>
+  )
+}
+
+export default App
---
+++ react-state-class_2/src/TodoList.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function TodoList(){
+    let [todos,setTodos]=useState(["sample task"]);
+    let [newTodo,setNewTodo]=useState("");
+
+    let addNewTask=()=>{
+        setTodos([...todos, newTodo]);
+        setNewTodo("");
+    }
+
+    let updateTodoValue = (event)=> {
+        setNewTodo(event.target.value);
+    }
+
+    return(
+        <div>
+            <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}/>
+            <br></br>
+            <button onClick={addNewTask}>Add Task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <hr></hr>
+            <h4>Tasks Todo</h4>
+            <ul>
+                {
+                    todos.map((todo)=>(
+                        <li>{todo}</li>
+                    ))
+                }
+            </ul>
+
+        </div>
+    );
+}
